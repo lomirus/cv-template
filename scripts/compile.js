d38133aebb85e18d9074f2e512eb819a003fc206
lumingrui <lomirus@qq.com>
2023-02-27 23:31:49 +0800

    Rename `cv.md` to `content.md`

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,7 +1,7 @@
 import { readFile, writeFile } from 'node:fs/promises'
 
 export default async function compile() {
-    const content = await readFile('./template/cv.md', 'utf-8');
+    const content = await readFile('./template/content.md', 'utf-8');
     const info = JSON.parse(await readFile('./template/info.json', 'utf-8'));
 
     await writeFile(
