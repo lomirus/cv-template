36c517b8fd35c8aea973f955da6931b8a50e25e4
lumingrui <lomirus@qq.com>
2023-02-27 23:30:39 +0800

    Implement `compile()`

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,3 +1,11 @@
-export default function compile() {
-    console.log("compiled")
+import { readFile, writeFile } from 'node:fs/promises'
+
+export default async function compile() {
+    const content = await readFile('./template/cv.md', 'utf-8');
+    const info = JSON.parse(await readFile('./template/info.json', 'utf-8'));
+
+    await writeFile(
+        './src/_template.ts',
+        `export default ${JSON.stringify({ info, content })}`
+    )
 }
