1584f99e14e388c85b7c5585e1134e62fcda9612
lumingrui <lomirus@qq.com>
2023-02-27 23:45:12 +0800

    Change the `_template.ts` generation

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,11 +1,11 @@
 import { readFile, writeFile } from 'node:fs/promises'
 
 export default async function compile() {
-    const content = await readFile('./template/content.md', 'utf-8');
-    const info = JSON.parse(await readFile('./template/info.json', 'utf-8'));
+    const content = (await readFile('./template/content.md', 'utf-8'));
+    const info = await readFile('./template/info.json', 'utf-8');
 
     await writeFile(
         './src/_template.ts',
-        `export default ${JSON.stringify({ info, content })}`
+        `export const info = ${info};\n\nexport const content = \`${content}\``
     )
 }
