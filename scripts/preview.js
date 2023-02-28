098da44992c958583a527ace86131a0dc8b2306a
lumingrui <lomirus@qq.com>
2023-02-28 22:17:41 +0800

    Add Windows support

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,4 +1,6 @@
 import { watch } from 'node:fs/promises'
+import { createServer } from 'vite'
+
 import compile from './compile.js';
 
 let isChanged = true;
@@ -10,9 +12,11 @@ setInterval(() => {
     }
 }, 200)
 
-const events = watch('./template/')
+const server = await createServer();
+await server.listen();
+await server.printUrls();
 
+const events = watch('./template/')
 for await (const _ of events) {
     isChanged = true
 }
-
