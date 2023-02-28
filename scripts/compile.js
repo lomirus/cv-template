57b8bcb106b9a1a3f23efa5af94abbd9e6f4b2b3
lumingrui <lomirus@qq.com>
2023-02-28 16:14:51 +0800

    Remove extra parentheses

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,7 +1,7 @@
 import { readFile, writeFile } from 'node:fs/promises'
 
 export default async function compile() {
-    const content = (await readFile('./template/content.md', 'utf-8'));
+    const content = await readFile('./template/content.md', 'utf-8');
     const info = await readFile('./template/info.json', 'utf-8');
 
     await writeFile(
